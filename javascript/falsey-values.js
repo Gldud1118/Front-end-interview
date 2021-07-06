cb774c0d2de548a39a568c57d77c2f13e28b5720
Gldud1118 <dud171111@gmail.com>
2021-07-06 11:07:03 +0900

    불리언 문맥에서 false로 평가되는 거짓 같은 값(falsey)

---
+++ javascript/falsey-values.js
@@ -0,0 +1,17 @@
+const example1 = undefined;
+const example2 = NaN;
+const example3 = null;
+const example4 = 0;
+const example5 = '';
+const example6 = false;
+const example7 = 0n;
+const example8 = -0;
+
+console.log(!!example1);
+console.log(!!example2);
+console.log(!!example3);
+console.log(!!example4);
+console.log(!!example5);
+console.log(!!example6);
+console.log(!!example7);
+console.log(!!example8);
