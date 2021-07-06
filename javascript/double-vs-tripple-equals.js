32b445b7bcefaba712c955fe12f2fa716c59cd21
Gldud1118 <dud171111@gmail.com>
2021-07-06 15:52:21 +0900

    강제(암시적 형변환을 일으켜) 값만 비교하는 == 연산자와 엄격하게 값과 타입을 모두 비교하는 === 연산자

---
+++ javascript/double-vs-tripple-equals.js
@@ -0,0 +1,22 @@
+console.log(5 == '5'); //true
+console.log(typeof 5);
+console.log(typeof '5');
+console.log(5 === '5'); //false
+
+console.log(false == 0); //true
+console.log(typeof false);
+console.log(typeof 0);
+console.log(false === 0); //false
+
+console.log(undefined == null); //true
+console.log(undefined === null); //false
+
+console.log('' == false); //true
+console.log('' === false); //false
+
+console.log(-0 == false); //true
+console.log(-0 === false); //false
+
+console.log(NaN == false); //false
+console.log(undefined == 0); //false
+console.log(null == 0); //false
