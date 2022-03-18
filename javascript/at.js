3b7882e5ba0059327611fec9ea98fd1ddbff5ecd
clevyoung <clevyoung@gmail.com>
2022-03-18 23:19:56 +0900

    정수 값을 받아, 배열에서 해당 값에 해당하는 인덱스의 요소를 반환하는 at 메서드

---
+++ javascript/at.js
@@ -0,0 +1,13 @@
+// const arr = [1, 2, 3, 4];
+// arr[arr.length - 2]; // 3
+// arr.slice(-2)[0]; // 3
+
+// const str = '1234';
+// str[str.length - 2]; // '3'
+// str.slice(-2)[0]; // '3'
+
+const arr = [1, 2, 3, 4];
+arr.at(-2); // 3
+
+const str = '1234';
+str.at(-2); // '3'
